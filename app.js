6d24b955045c95237005e8aaa7f72d37aafb1718
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2022-11-29 19:26:51 +0100

    Added language and speech speed option

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 let textArea = document.getElementById("text")
 let activeVoiceBtn = document.getElementsByTagName("button")[0]
+let voiceLang = document.getElementById("txt-lang")
+let speechSpeed = document.getElementById("speech-speed")
+let showSpeechSpeed = document.getElementById("speech-speed-value")
 
+speechSpeed.addEventListener("mousemove", function() {
+    showSpeechSpeed.innerHTML = speechSpeed.value
+})
 activeVoiceBtn.addEventListener("click", textToSpeach)
 
-function textToSpeach(){
 
+function textToSpeach(){
     let read = new SpeechSynthesisUtterance()
     read.text = textArea.value
-    read.lang = "pl-PL"
-    read.rate = 1.2
+    read.lang = voiceLang.value
+    read.rate = speechSpeed.value
     speechSynthesis.speak(read)
-    console.log(read)
 }
