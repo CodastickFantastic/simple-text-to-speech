9c24d8e374ba8b55bf7d2ae9c4e3d794725ce579
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2022-11-30 20:15:34 +0100

    Added Speech to Text section

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+// Text to Speech Inputs
 let textArea = document.getElementById("text")
-let activeVoiceBtn = document.getElementsByTagName("button")[0]
+let speechToTextBtn = document.getElementsByTagName("button")[0]
 let speechSpeed = document.getElementById("speech-speed")
 let showSpeechSpeed = document.getElementById("speech-speed-value")
 let changeVoiceSelector = document.getElementById("change-voice")
 let voiceList =[]
+let read = new SpeechSynthesisUtterance()
+
+// Speech to Text
+let startRecordingBtn = document.getElementsByTagName("button")[1]
+let stopRecordingBtn = document.getElementsByTagName("button")[2]
+let showVoiceRecogantionArea = document.getElementById("collected-text")
+let recordingLanguage = document.getElementById("recording-language")
+//Withouth following snippet SpeechRecognation() is not defined
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+
+
+//------------------------------Section1: Text to Speech Code------------------------------
 
 // Without this snippet getVoices() return null array
 window.onload = function() {
    speechSynthesis.getVoices()
 }
-    
+
+//Main events
 setTimeout(showVoices, 50)
 speechSpeed.addEventListener("mousemove", function() {
     showSpeechSpeed.innerHTML = speechSpeed.value
 })
-activeVoiceBtn.addEventListener("click", textToSpeach)
+speechToTextBtn.addEventListener("click", textToSpeach)
 
 
 function showVoices(){
@@ -29,16 +44,41 @@ function showVoices(){
 }
 
 function textToSpeach(){
-    let read = new SpeechSynthesisUtterance()
     let voice = changeVoiceSelector.value
-    
+
     for(let i = 0; i < voiceList.length; i++){
         if (voiceList[i].name === voice){
             read.voice = voiceList[i]
         }
     }
-
     read.text = textArea.value
     read.rate = speechSpeed.value
     speechSynthesis.speak(read)
 }
+
+//------------------------------Section 2: Speech to Text------------------------------
+
+startRecordingBtn.addEventListener("click", function() {
+    recognition.onerror = function(event){alert(event.error)}
+    recognition.onend = function(){recognition.start()}
+    let collectedText = ""
+    recognition.onresult = function(event){
+        if(!event){
+            console.log("a")
+        } else {
+            console.log("b")
+        }
+
+        if(event.results.length > 0){
+            collectedText += " " +event.results[0][0].transcript
+            showVoiceRecogantionArea.value = collectedText
+        }
+    }
+    recognition.lang = recordingLanguage.value
+    recognition.start()
+})
+
+stopRecordingBtn.addEventListener("click", function(){
+    recognition.onend = function(){recognition.stop()}
+    recognition.stop()
+})
